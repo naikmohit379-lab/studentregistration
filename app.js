56edf902e6c3ca4ecf71585bb8398bd908a4c4cb
naikmohit379-lab <naikmohit379@gmail.com>
2026-04-15 17:26:15 +0530

    selected the html element which needs to be changed

---
+++ app.js
@@ -0,0 +1,13 @@
+// to store the student details 
+let student =[];
+
+
+// selection of html elements 
+const studentForm = document.getElementById("studentForm");
+const studentName = document.getElementById("studentName");
+const studentId = document.getElementById("studentId");
+const email = document.getElementById("email");
+const contact = document.getElementById("contact");
+const submitBtn = document.getElementById("submitBtn");
+const tableBody = document.querySelector("#studentsTable tbody");
+const tableWrapper = document.getElementById("tableWrapper");
